f0f129fec0b2e9c038e87f969d17cfad5139f92b
Thomas Kint <thomaskint.pro@gmail.com>
2021-01-31 17:54:41 +0100

    refacto: init v2

--- src/components/pages/redirection/index.js
+++
@@ -1,7 +0,0 @@
-import Error404 from './Error404';
-import ComingSoon from './ComingSoon';
-
-export default {
-  Error404,
-  ComingSoon,
-};
--- src/components/template/index.js
+++
@@ -1,7 +0,0 @@
-import Toolbar from './components/toolbar/Toolbar';
-import Footer from './components/footer/Footer';
-
-export default {
-  Toolbar,
-  Footer,
-};
